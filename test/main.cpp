17b76391e9b56ebfabd67040130f54a6843ce51a
Elviss Strazdins <elviss@elviss.lv>
2020-05-30 00:20:34 +0300

    Implement test

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 #include "filestdio.hpp"
 
 int main(int argc, const char * argv[])
 {
+    try
+    {
+        {
+            filestdio::Redirect out("test.txt", filestdio::Stream::out);
+
+            std::cout << "test";
+        }
+
+        filestdio::Redirect in("test.txt", filestdio::Stream::in);
+
+        std::string s;
+        std::cin >> s;
+
+        if (s != "test")
+            throw std::runtime_error("Wrong string");
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
